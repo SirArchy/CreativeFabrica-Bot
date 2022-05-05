dffdb70b7d099569cf0aaac8b2f5a658679036a1
SirArchy <99512652+SirArchy@users.noreply.github.com>
2022-05-05 11:57:16 +0200

    reverted timer code

--- download_from_creativefabrica.py
+++ download_from_creativefabrica.py
@@ -1,19 +1,5 @@
 import requests
 import re
-from datetime import datetime, timedelta
-from threading import Timer
-
-x=datetime.today()
-y = x.replace(day=x.day, hour=10, minute=0, second=0, microsecond=0) + timedelta(days=1)
-delta_t=y-x
-secs=delta_t.total_seconds()
-t1 = Timer(secs, download_file('https://www.creativefabrica.com/de/cfsecure/download/?dg=13603093&_wpnonce=9ac01f884b'))
-t2 = Timer(secs, download_file('https://www.creativefabrica.com/de/cfsecure/download/?dg=28114472&_wpnonce=5d584069cf'))
-t3 = Timer(secs, download_file('https://www.creativefabrica.com/de/cfsecure/download/?dg=28593430&_wpnonce=f2c3d96ba1'))
-t1.start()
-t2.start()
-t3.start()
-
 
 def getFilename_fromCd(cd):
 #Get filename from content-disposition
@@ -28,3 +14,7 @@ def download_file(url):
     r = requests.get(url, allow_redirects=True)
     filename = getFilename_fromCd(r.headers.get('content-disposition'))
     open(filename, 'wb').write(r.content)
+
+download_file('https://www.creativefabrica.com/de/cfsecure/download/?dg=13603093&_wpnonce=9ac01f884b')
+download_file('https://www.creativefabrica.com/de/cfsecure/download/?dg=28114472&_wpnonce=5d584069cf')
+download_file('https://www.creativefabrica.com/de/cfsecure/download/?dg=28593430&_wpnonce=f2c3d96ba1')
